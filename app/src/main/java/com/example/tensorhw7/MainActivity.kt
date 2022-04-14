8a4302bca491caf59c6317eed8d9523ea4a993b0
Konstantin Antonov <koantonov99@gmail.com>
2022-04-14 22:56:49 +0700

    Add AnimationsFragment

--- app/src/main/java/com/example/tensorhw7/MainActivity.kt
+++ app/src/main/java/com/example/tensorhw7/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.tensorhw7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import com.example.tensorhw7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.animationsButton.setOnClickListener {
+            binding.animationsButton.isGone = true
+            addFragment(AnimationsFragment())
+        }
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        binding.animationsButton.isGone = false
     }
 }
